b6cf144fb8fc71b8a06ea25e1ca6d5f09be88827
Agatolies <laure.deste@gmail.com>
2022-10-01 20:38:05 +0200

    Add IBodyRockyApi with Refit (a nuget package)

---
+++ src/BodyRocky.Client/IBodyRockyApi.cs
@@ -0,0 +1,27 @@
+﻿using BodyRocky.Core.Contracts.Requests.CustomerRequests;
+using BodyRocky.Core.Contracts.Responses.CustomerResponses;
+using Refit;
+
+namespace BodyRocky.Client;
+
+public interface IBodyRockyApi
+{
+    #region Customers
+
+    [Get("/customers")]
+    Task<GetAllCustomersResponse> GetCustomersAsync();
+    
+    [Get("/customers/{request.CustomerId}")]
+    Task<CustomerResponse> GetCustomerAsync(GetCustomerRequest request);
+    
+    [Post("/customers")]
+    Task<CustomerResponse> CreateCustomerAsync([Body] CreateCustomerRequest request);
+    
+    [Put("/customers")]
+    Task<CustomerResponse> UpdateCustomerAsync([Body] UpdateCustomerRequest request);
+    
+    [Delete("/customers/{request.CustomerId}")]
+    Task DeleteCustomerAsync(DeleteCustomerRequest request);
+
+    #endregion
+}
