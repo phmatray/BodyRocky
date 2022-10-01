f02be6204ed0468a48dd69d0a32660029b47de13
Agatolies <laure.deste@gmail.com>
2022-10-01 20:39:58 +0200

    Create BodyRockyClient in the web application

--- src/BodyRocky.Front.WebApp/Program.cs
+++ src/BodyRocky.Front.WebApp/Program.cs
@@ -1,18 +1,24 @@
 using Microsoft.AspNetCore.Components.Web;
 using Microsoft.AspNetCore.Components.WebAssembly.Hosting;
 using BodyRocky.Front.WebApp;
+using Refit;
 
 var builder = WebAssemblyHostBuilder.CreateDefault(args);
+var services = builder.Services;
 builder.RootComponents.Add<App>("#app");
 builder.RootComponents.Add<HeadOutlet>("head::after");
 
-builder.Services.AddScoped(sp => new HttpClient { BaseAddress = new Uri(builder.HostEnvironment.BaseAddress) });
+services.AddScoped(sp => new HttpClient { BaseAddress = new Uri(builder.HostEnvironment.BaseAddress) });
 
-builder.Services.AddOidcAuthentication(options =>
+services.AddOidcAuthentication(options =>
 {
     // Configure your authentication provider options here.
     // For more information, see https://aka.ms/blazor-standalone-auth
     builder.Configuration.Bind("Local", options.ProviderOptions);
 });
+
+services
+    .AddRefitClient<IBodyRockyApi>()
+    .ConfigureHttpClient(c => c.BaseAddress = new Uri("https://localhost:7180"));
 
 await builder.Build().RunAsync();
